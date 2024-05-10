238b9e0dcb21d0c309a612c0eb9a1b8cca14b7e5
onesBenRhaime <onesrhaime28@gmail.com>
2024-05-10 18:08:00 +0100

    vf

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,20 +3,33 @@ import { RouterModule, Routes } from '@angular/router';
 import { FrontLayoutComponent } from './layouts/front-layout/front-layout.component';
 
 const routes: Routes = [
-  {path:"", component:FrontLayoutComponent, children:[
-       {path:"", loadChildren:()=>import("./views/front/home/home.module").then(m=>m.HomeModule)},   
-       {path:"contact", loadChildren:()=>import("./views/front/contact/contact.module").then(m=>m.ContactModule)},
-       {path:"about", loadChildren:()=>import("./views/front/about/about.module").then(m=>m.AboutModule)},
-       {path:"products", loadChildren:()=>import("./views/front/products/products.module").then(m=>m.ProductsModule)},
-       {path:"products/details", loadChildren:()=>import("./views/front/products/products.module").then(m=>m.ProductsModule)},
-       {path:"categories", loadChildren:()=>import("./views/front/categorie/categorie.module").then(m=>m.CategorieModule)},
-       {path:"service", loadChildren:()=>import("./views/front/service/service.module").then(m=>m.ServiceModule)},
-  ]},
- 
+  {
+    path: '',
+    component: FrontLayoutComponent,
+    children: [
+      {
+        path: '',
+        loadChildren: () =>
+          import('./views/front/home/home.module').then((m) => m.HomeModule),
+      },
+      {
+        path: 'contact',
+        loadChildren: () =>
+          import('./views/front/contact/contact.module').then(
+            (m) => m.ContactModule
+          ),
+      },
+      {
+        path: 'about',
+        loadChildren: () =>
+          import('./views/front/about/about.module').then((m) => m.AboutModule),
+      },
+    ],
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
